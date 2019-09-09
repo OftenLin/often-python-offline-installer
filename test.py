b10e7d4140e425e6636bf6a69daf71f19ac48768
Often <often@cht.com.tw>
2019-09-09 11:39:14 +0800

    Add test example

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import re
+import datetime
+import pandas as pd
+import zipcodetw
+from zipcodetw.util import Address
+from zipcodetw.util import Rule
+from os.path import dirname, join
+import sqlite3
+from collections import defaultdict
+import psycopg2
+import codecs
+import logging
+
+
+print('Test pass!')
