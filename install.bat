cf3e38db1c9ae9899fb3250c9067205d5fb41930
Often <often@cht.com.tw>
2019-09-09 14:42:03 +0800

    Correct the comment way in batch file

--- install.bat
+++ install.bat
@@ -1,19 +1,19 @@
 echo off
-:Close the log when execution
+:: Close the log when execution
 
 call :LOG > install.log
 exit /B
-:generate log
+:: generate log
 
 
 :LOG
 set install_folder=%~dp0material\3.6
-:find out your environment current version to set the mapping folder, ex python 3.6 -> material\3.6
+:: find out your environment current version to set the mapping folder Ex: python 3.6 -> material\3.6
 echo %install_folder%
-:make sure the cmd location is in material
+:: make sure the cmd location is in material
 
 cd /d %install_folder%
-: cd to current parent folder to cd later
+:: cd to current parent folder to cd later
 
 for /f "delims=" %%i in ('dir /B /N') do (
 	if exist %%~si\NUL (
@@ -24,10 +24,10 @@ for /f "delims=" %%i in ('dir /B /N') do (
 		pip install %%i
 	)
 )
-: pip install folder need python setup.py but file only pip required
+:: pip install folder need python setup.py but file only pip required
 
 
-: for %%i in (*) do pip install %%i :pip install file only show for file so can install directly
+:: for %%i in (*) do pip install %%i :pip install file only show for file so can install directly
 
 
 cd %~dp0
